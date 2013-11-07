46fdab552f1ec87a9ec2494da78e1b0c2769a16a
SE User <seuser@haskellfp>
2013-11-07 17:28:52 -0500

    Creacion del proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Openpm::Application.config.secret_key_base = '9864f3c8fb3460549c9837309dca02efd1acacc3ed1f8afb24231b88324adc45ba996a604888e527b00c4c93906b73830fb1c4aa19a74f7b8bb96d18f8d2f9f9'
