442c8ef3ab0cff7ea321d2c247b4db7a07f4c8c0
SE User <seuser@haskellfp>
2013-11-20 16:29:59 -0500

    fix belongs_to relationship save

--- app/models/proyect.rb
+++ app/models/proyect.rb
@@ -1,7 +1,7 @@
 class Proyect < ActiveRecord::Base
   belongs_to :customer
 
-  validates :name, :description, presence: true;
+  validates :customer, :name, :description, presence: true;
   validates_inclusion_of :state, in: %w( proposal contract started finish )
   validates_associated :customer
 end
