3377566c8c5361aa56486fb38cef9b50e6669700
SE User <seuser@haskellfp>
2014-01-19 11:45:39 -0500

    work advance

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,9 +3,15 @@ require 'spec_helper'
 describe Project do
   #pending "add some examples to (or delete) #{__FILE__}"
 
-  it 'proyect with correct values must be created' do
+  it 'should be create a valid project' do
     customer_se_test = Customer.create!(nif: '9999999999999', name: 'SE Test Company')
-    proyect = Project.create!(name: 'Proyect Name', description: 'Proyect Description', customer: customer_se_test)
+    proyect = Project.create!(
+        name: 'Proyect Name',
+        description: 'Proyect Description',
+        customer: customer_se_test,
+        work_mode: 'by_delivery',
+        value: 4000.45
+    )
     expect(Project).to have(1).record
   end
 end
